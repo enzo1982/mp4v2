23548c021633d5b7ad14d4474e46b3acafb198c3
Kona8lend <Kona8lend@6e6572fa-98a6-11dd-ad9f-f77439c74b79>
2008-12-06 15:05:04 +0000

    Win32 - updated (cached) project.h file.

--- vstudio9.0/include/mp4v2/project.h
+++ vstudio9.0/include/mp4v2/project.h
@@ -6,13 +6,13 @@
 #define MP4V2_PROJECT_name            "MP4v2"
 #define MP4V2_PROJECT_name_lower      "mp4v2"
 #define MP4V2_PROJECT_name_upper      "MP4V2"
-#define MP4V2_PROJECT_name_formal     "MP4v2 2.0-r102"
+#define MP4V2_PROJECT_name_formal     "MP4v2 2.0-r125"
 #define MP4V2_PROJECT_url_website     "http://code.google.com/p/mp4v2"
 #define MP4V2_PROJECT_url_downloads   "http://code.google.com/p/mp4v2/downloads/list"
 #define MP4V2_PROJECT_url_discussion  "http://groups.google.com/group/mp4v2"
 #define MP4V2_PROJECT_irc             "irc://irc.freenode.net/handbrake"
 #define MP4V2_PROJECT_bugreport       "<eddyg@myreflection.org>"
-#define MP4V2_PROJECT_version         "2.0-r102"
+#define MP4V2_PROJECT_version         "2.0-r125"
 #define MP4V2_PROJECT_version_hex     0x00020000
 #define MP4V2_PROJECT_version_major   2
 #define MP4V2_PROJECT_version_minor   0
@@ -20,10 +20,10 @@
 #define MP4V2_PROJECT_repo_url        "https://mp4v2.googlecode.com/svn/trunk"
 #define MP4V2_PROJECT_repo_root       "https://mp4v2.googlecode.com/svn"
 #define MP4V2_PROJECT_repo_uuid       "6e6572fa-98a6-11dd-ad9f-f77439c74b79"
-#define MP4V2_PROJECT_repo_rev        102
-#define MP4V2_PROJECT_repo_date       "2008-11-27 21:48:46 -0500 (Thu, 27 Nov 2008)"
+#define MP4V2_PROJECT_repo_rev        125
+#define MP4V2_PROJECT_repo_date       "2008-12-06 09:44:52 -0500 (Sat, 06 Dec 2008)"
 #define MP4V2_PROJECT_repo_type       "developer"
-#define MP4V2_PROJECT_build           "Sat Nov 29 00:35:03 EST 2008"
+#define MP4V2_PROJECT_build           "Sat Dec  6 10:04:15 EST 2008"
 
 /*****************************************************************************/
 
