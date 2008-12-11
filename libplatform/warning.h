641bad92e82cf2a5be4551d5578d655681d311c0
Kona8lend <Kona8lend@6e6572fa-98a6-11dd-ad9f-f77439c74b79>
2008-12-11 02:21:34 +0000

    VStudio - disabled VStudio warning about templates and DLL export - added new file src/mp4error.h to solution.

--- libplatform/warning.h
+++ libplatform/warning.h
@@ -9,6 +9,7 @@
 #   pragma GCC diagnostic ignored "-Wwrite-strings"
 #elif defined( _MSC_VER )
 #   pragma warning( disable: 4244 )
+#   pragma warning( disable: 4251 )
 #   pragma warning( disable: 4800 )
 #   pragma warning( disable: 4996 )
 #endif
