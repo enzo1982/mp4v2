08d14fd76ca8ea16ac1e0c7cb969c1965fbf9f10
Kona8lend <Kona8lend@6e6572fa-98a6-11dd-ad9f-f77439c74b79>
2009-03-07 18:37:53 +0000

    - fixed transposed memset() parameters; thanks @j45

--- src/itmf/Tags.cpp
+++ src/itmf/Tags.cpp
@@ -54,7 +54,7 @@ void
 Tags::c_alloc( MP4Tags*& tags )
 {
     tags = new MP4Tags();
-    memset( tags, sizeof(Tags), 0 ); // safe: pure C-struct
+    memset( tags, 0, sizeof(Tags) ); // safe: pure C-struct
     tags->__handle = this;
 }
 
