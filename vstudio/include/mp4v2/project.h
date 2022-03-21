f95da887c8579623c3c6df0445ec6efea93f1433
Robert Kausch <robert.kausch@freac.org>
2022-03-22 00:00:48 +0100

    Bump version number for 2.1.1 release.

--- vstudio/include/mp4v2/project.h
+++ vstudio/include/mp4v2/project.h
@@ -6,19 +6,19 @@
 #define MP4V2_PROJECT_name            "MP4v2"
 #define MP4V2_PROJECT_name_lower      "mp4v2"
 #define MP4V2_PROJECT_name_upper      "MP4V2"
-#define MP4V2_PROJECT_name_formal     "MP4v2 2.1.0"
+#define MP4V2_PROJECT_name_formal     "MP4v2 2.1.1"
 #define MP4V2_PROJECT_url_website     "https://mp4v2.org"
 #define MP4V2_PROJECT_url_downloads   "https://github.com/enzo1982/releases"
 #define MP4V2_PROJECT_url_discussion  "https://github.com/enzo1982/discussions"
 #define MP4V2_PROJECT_url_bugreport   "https://github.com/enzo1982/issues"
 #define MP4V2_PROJECT_support         "<support@mp4v2.org>"
-#define MP4V2_PROJECT_version         "2.1.0"
-#define MP4V2_PROJECT_version_hex     0x00020100
+#define MP4V2_PROJECT_version         "2.1.1"
+#define MP4V2_PROJECT_version_hex     0x00020101
 #define MP4V2_PROJECT_version_major   2
 #define MP4V2_PROJECT_version_minor   1
-#define MP4V2_PROJECT_version_point   0
+#define MP4V2_PROJECT_version_point   1
 #define MP4V2_PROJECT_repo_url        "git@github.com:enzo1982/mp4v2.git"
-#define MP4V2_PROJECT_build           "Sun Jan 30 15:39:29     2022"
+#define MP4V2_PROJECT_build           "Mon Mar 21 23:53:15     2022"
 
 /*****************************************************************************/
 
